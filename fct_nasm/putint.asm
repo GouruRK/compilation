e12fc787d7dab439300344792f0e7feebd014d34
GouruRK <gouru.rk.gouru.rk@gmail.com>
2024-04-21 18:21:10 +0200

    correct int print

--- fct_nasm/putint.asm
+++ fct_nasm/putint.asm
@@ -5,6 +5,7 @@ section .text
 ; rax: int à afficher 
 ; rbx: diviseur
 ; rdx: reste de la division
+; r12: length
 putint:
     ; convention d'appel AMD64
     push    rbp
@@ -12,23 +13,34 @@ putint:
 
     mov     rax, rdi        ; sauvegarde de l'entier à afficher
     mov     rbx, 10         ; diviseur
+    mov     r12, 0
 
 loop:
     cmp     rax, 0
-    je      exit
+    je      print
 
     mov     rdx, 0          ; initialise le reste
 
     idiv    rbx             ; rax = rax // rbx, rdx = rax % rbx
     
-    push    rax
-
     add     rdx, '0'        ; rdx += '0'
-    mov     rdi, rdx        ; on affiche rax % 10
-    call    putchar 
+    
+    push    rdx
 
-    pop     rax
+    inc     r12
+
     jmp     loop
+
+print:
+
+    cmp     r12, 0
+    je      exit
+
+    pop     rdi
+    call    putchar
+
+    dec     r12
+    jmp     print
 
 exit:
     mov     rsp, rbp        ; restore pile
--- fct_nasm/test.c
+++ fct_nasm/test.c
@@ -3,18 +3,8 @@ char getchar();
 int getint();
 void putint(int);
 
-int main(){
-    // char a = getchar();
-    int b = getint();
-    // putchar(a);
-    // putchar('\n');
-    putint(b);
+int main() {
+    putint(getint());
     putchar('\n');
-    // putchar(a);
-    // putint(b);
-    // putchar('\n');
-    // putchar('p');
-    // putchar('\n');
-    // putchar('\n');
     return 0;
 }
